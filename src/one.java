bc1d183c9f41ff459b52c57a3f56906dd7b453e5
Deepak <jdeepak2256@gmail.com>
2025-10-24 18:44:15 +0530

    DAY 1 - 21-10-2025

--- src/one.java
+++ src/one.java
@@ -1,7 +1,7 @@
 public class one {
-        public static void main(String[] args) {
-            String s = "madam";
-            System.out.println(s.equals(new StringBuilder(s).reverse().toString()) ? "Palindrome" : "Not Palindrome");
-        }
+    public static void main(String[] args) {
+        String s = "madam";
+        System.out.println(s.equals(new StringBuilder(s).reverse().toString())
+                ? "Palindrome" : "Not Palindrome");
     }
 }
